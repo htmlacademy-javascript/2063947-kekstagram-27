3fd62faf4a5364112c81341896ca65b1fd639d99
Keks <keks@htmlacademy.ru>
2022-10-26 18:59:38 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+//import './data.js';
+//import {createPhotoDescriptions} from './data.js';
+//createPhotoDescriptions();
+
+//import './newphotos.js';
+import {createUserPhotos} from './newphotos.js';
+createUserPhotos();
+//console.log(createUserPhotos());
