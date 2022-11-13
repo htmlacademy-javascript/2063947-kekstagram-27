1b3e501bf9ddab95f2b2cabd020ff5d50b778f4a
romanetsinna <106196773+romanetsinna@users.noreply.github.com>
2022-11-13 03:11:40 -0500

    Помощь друга (#9)
    * Модуляция
    
    * Модуляция
    
    * Отрисуй меня полностью (часть 1)
    
    * Отрисуй меня полностью (1_1)
    
    * Отрисуй меня полностью 2
    
    * Отрисуй меня полностью 2_1
    
    * Отрисуй меня полностью 2_1
    
    * Правда или действие 1
    
    * Правда или действие 1.1
    
    * Правда или действие 1.1
    
    * Правда или действие 1.1
    
    * Правда или действие 1
    
    * Правда или действие 1
    
    * Правда или действие 2
    
    * Правда или действие 2
    
    * Правда или действие 2
    
    * Правда или действие 2
    
    * Помощь друга
    
    * Помощь друга
    
    * Помощь друга
    
    * Помощь друга
    
    Co-authored-by: Ruslan Breev <32988951+blckwltz@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import {createUserPhotos} from './newphotos.js';
 createUserPhotos();
 
 import './forms.js';
+import './slider.js';
---
+++ js/slider.js
@@ -0,0 +1,146 @@
+import {removeLastCharacter} from './util.js';
+import {toNumber} from './util.js';
+
+//изменение масштаба
+const zoomOutButton = document.querySelector('.scale__control--smaller');
+const zoomInButton = document.querySelector('.scale__control--bigger');
+const scaleInput = document.querySelector('input[name="scale"]');
+const imagePreview = document.querySelector('.img-upload__preview img');
+const scaleStep = 25;
+const scaleMin = 25;
+const scaleMax = 100;
+scaleInput.value = '100%';
+
+zoomInButton.addEventListener('click', () => {
+  const currentValue = scaleInput.value;
+  const numberValue = toNumber(removeLastCharacter(currentValue));
+
+  if (numberValue < scaleMax) {
+    const newValue = numberValue + scaleStep;
+
+    scaleInput.value = `${newValue}%`;
+    imagePreview.style.transform = `scale(${newValue / 100})`;
+  }
+});
+
+zoomOutButton.addEventListener('click', () => {
+  const currentValue = scaleInput.value;
+  const numberValue = toNumber(removeLastCharacter(currentValue));
+
+  if (numberValue > scaleMin) {
+    const newValue = numberValue - scaleStep;
+
+    scaleInput.value = `${newValue}%`;
+    imagePreview.style.transform = `scale(${newValue / 100})`;
+  }
+});
+
+//применение слайдера
+const sliderElement = document.querySelector('.effect-level__slider');
+const valueElement = document.querySelector('.effect-level__value');
+const fieldsetElement = document.querySelector('.img-upload__effects');
+imagePreview.classList.add('effects__preview--none'); //эффект Оригинал по умолчанию
+valueElement.value = 100; //сброс уровня насыщенности
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 50,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+fieldsetElement.addEventListener('change', (evt) => {
+  if (evt.target.value === 'none') {
+    imagePreview.style.filter = '';
+    sliderElement.classList.add('hidden');
+  } else if (evt.target.value === 'chrome') {
+    sliderElement.classList.remove('hidden');
+    imagePreview.className = `effects__preview--${evt.target.value}`;
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 0.5,
+      step: 0.1,
+    });
+    sliderElement.noUiSlider.on('update', () => {
+      valueElement.value = sliderElement.noUiSlider.get();
+      imagePreview.style.filter = `grayscale(${valueElement.value})`; //хром
+    });
+  } else if (evt.target.value === 'sepia') {
+    sliderElement.classList.remove('hidden');
+    imagePreview.className = `effects__preview--${evt.target.value}`;
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 0.5,
+      step: 0.1,
+    });
+    sliderElement.noUiSlider.on('update', () => {
+      valueElement.value = sliderElement.noUiSlider.get();
+      imagePreview.style.filter = `sepia(${valueElement.value})`; //сепия
+    });
+  } else if (evt.target.value === 'marvin') {
+    sliderElement.classList.remove('hidden');
+    imagePreview.className = `effects__preview--${evt.target.value}`;
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: 1,
+    });
+    sliderElement.noUiSlider.on('update', () => {
+      valueElement.value = sliderElement.noUiSlider.get();
+      imagePreview.style.filter = `invert(${valueElement.value}%)`; //марвин
+    });
+  } else if (evt.target.value === 'phobos') {
+    sliderElement.classList.remove('hidden');
+    imagePreview.className = `effects__preview--${evt.target.value}`;
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 3,
+      },
+      start: 1.5,
+      step: 0.1,
+    });
+    sliderElement.noUiSlider.on('update', () => {
+      valueElement.value = sliderElement.noUiSlider.get();
+      imagePreview.style.filter = `blur(${valueElement.value}px)`; //фобос в px
+    });
+  } else if (evt.target.value === 'heat') {
+    sliderElement.classList.remove('hidden');
+    imagePreview.className = `effects__preview--${evt.target.value}`;
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: 1,
+        max: 3,
+      },
+      start: 2,
+      step: 0.1,
+    });
+    sliderElement.noUiSlider.on('update', () => {
+      valueElement.value = sliderElement.noUiSlider.get();
+      imagePreview.style.filter = `brightness(${valueElement.value})`; //зной
+    });
+  }
+});
